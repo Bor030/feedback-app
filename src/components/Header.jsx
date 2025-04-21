66d7a41455ebf04c8a0f28bc7392384b9238cd9e
Aleksandar030 <alexsakic@yahoo.com>
2025-04-21 22:58:25 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+
+
+  return (
+    <header>
+      <div className='container' >
+        
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  )
+}
+
+
+
+export default Header
+
+
+
